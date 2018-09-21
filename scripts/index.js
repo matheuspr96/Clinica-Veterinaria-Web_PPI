3833c0b3fbbb0520cd3575df2411c324c0a3e59c
Gabriel Pinheiro <bielrsgc@gmail.com>
2018-09-21 14:05:51 -0300

    Item selecionado na navbar agora aparece destacado

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,10 @@ $(() => {
 	$("[data-mostra]").click(e => {
 		// Esconde cartoes
 		$(".cartao").removeClass("cartao_visivel");
-
+		// Desseleciona item selecionado
+		$("[data-mostra]").removeClass("barra-topo__item_selecionado");
+		// Seleciona novo item
+		$(e.target).addClass("barra-topo__item_selecionado");
 		// Mostra cartao desejado
 		$(".cartao_pag_" + $(e.target).data("mostra")).addClass("cartao_visivel");
 	});
