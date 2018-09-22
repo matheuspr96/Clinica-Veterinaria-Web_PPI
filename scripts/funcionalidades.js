2897090e6cc55715e26d23e0190336df1b0aa102
matheuspr96 <matheuspr96@hotmail.com>
2018-09-22 19:07:04 -0300

    html de funcionalidades foi criado, e tabela de listar funcionarios responsiva

---
+++ scripts/funcionalidades.js
@@ -0,0 +1,35 @@
+$(() => {
+	var isGaleriaCarregada = false; // Guarda se as imagens da galeria já foram carregadas ou não (para exibir a animação)
+
+	/**
+	 * Menu navbar
+	 */
+	$("[data-mostra]").click(e => {
+		let mostra = $(e.target).data("mostra");
+		// Esconde cartoes
+		$(".cartao").removeClass("cartao_visivel");
+		// Desseleciona item selecionado
+		$("[data-mostra]").removeClass("barra-topo__item_selecionado");
+		// Seleciona novo item
+		$(`.barra-topo__item[data-mostra="${mostra}"]`).addClass("barra-topo__item_selecionado");
+		// Mostra cartao desejado
+		$(".cartao_pag_" + mostra).addClass("cartao_visivel");
+    });
+    
+    $(".mostra-login").click(e => $("#login-dialog").toggleClass("dialog_escondido"));
+
+	/**
+	 * Dialogs
+	 */
+	$(".dialog__fundo").click(e => $(e.target.parentNode).addClass("dialog_escondido"));
+
+	/**
+	 * Menu
+	 */
+    $("#abre-menu, .menu__fechar, .menu__fundo, .menu__item").click(e => $(".menu").toggleClass("menu_escondido"));
+
+	/**
+	 * Footer
+	 */
+	$(".footer").text(`\u00A9 ${(new Date()).getFullYear()} Zika-PET`);
+});
