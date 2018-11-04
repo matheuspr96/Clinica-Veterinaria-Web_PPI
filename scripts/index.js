09fb9d1162473b68adaefd234a7501c2e1b03a19
matheuspr96 <matheuspr96@hotmail.com>
2018-11-04 18:47:32 -0200

    update

--- scripts/index.js
+++ scripts/index.js
@@ -209,27 +209,7 @@ $(() => {
 		data: {'agendamento__especialidade':agendamento__especialidade},         
   
 		success: function(result) {
-		
-		  // se dataType fosse 'html', então seria necessário
-		  // converter a string JSON recebida em um objeto JavaScript
-		  // manualmente (utilizando a função JavasScript JSON.parse)
-		  
-		  // Como dataType foi definido para o valor 'json', então a conversão
-		  // da string para um objeto JavaScript é realizada automaticamente.
-  
-		  // NOTA IMPORTANTE 1: Entretanto, todo conteúdo gerado
-		  // pelo script PHP precisa ser convertido para JSON (no servidor, em PHP).
-		  // Caso contrário, teremos um erro de conversão para 
-		  // JSON no JavaScript/jQuery, o que faz com que esta parte
-		  // do código (success:) não seja executada, mas sim a parte 
-		  // definida em 'error:'. Isto pode acontecer mesmo
-		  // quando o script PHP termina sem gerar erros.
-  
-		  // NOTA IMPORTANTE 2: Em algumas situações esta funçao pode ser
-		  // executada mesmo quando o script PHP não termina com sucesso 
-		  // (por exemplo, quando ocorre erros de sintaxe no PHP). Isto acontece
-		  // porque em algumas situações o PHP (em conjunto com o servidor web) retorna
-		  // o código de STATUS 200 - OK, mesmo na presenças de alguns erros/warnings no script.       
+     
 		if (result != "")
 		{         
 
