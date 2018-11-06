bb83cf77703979f4c835ea54fa6a9db786bf9a96
matheuspr96 <matheuspr96@hotmail.com>
2018-11-06 01:59:28 -0200

    busca endereco

---
+++ php/busca_endereco.php
@@ -0,0 +1,61 @@
+<?php
+
+    class Endereco 
+    {
+    public $rua;
+    public $bairro;
+    public $cidade;
+    }
+
+    try
+    {
+        $msgErro = "";
+        require "conexaoBanco.php"; 
+        $conn = conectaMySQL();
+        $endereco = $cep = "";
+        if (isset($_POST["cep"]))
+        $cep = $_POST["cep"];
+
+        $SQL = "
+        SELECT Rua, Bairro, Cidade
+        FROM Endereco
+        WHERE Cep = ?;
+        ";
+
+        if (! $stmt = $conn->prepare($SQL))
+            throw new Exception("Falha na operacao prepare: " . $conn->error);
+
+        if (! $stmt->bind_param("s", $cep))
+            throw new Exception("Falha na operacao bind_param: " . $stmt->error);
+
+        // Executa a consulta
+        if (! $stmt->execute())
+            throw new Exception("Falha na operacao execute: " . $stmt->error);
+
+        // Indica as variáveis PHP que receberão os resultados
+        if (! $stmt->bind_result($rua, $bairro, $cidade))
+            throw new Exception("Falha na operacao bind_result: " . $stmt->error);  
+        
+        
+        while ($stmt->fetch())
+        {
+
+            $endereco = new Endereco();
+
+            $endereco->rua    = $rua;
+            $endereco->bairro = $bairro;
+            $endereco->cidade = $cidade;
+        } 
+
+        $jsonStr = json_encode($endereco);
+        echo $jsonStr;
+
+    }
+    catch (Exception $e)
+    {
+    $msgErro = $e->getMessage();
+    }
+    if ($conn != null)
+    $conn->close();
+
+?>
